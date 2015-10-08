556a527e0c3f635991d5b574e36decd22796fd88
Sven Rossmann <scrossmann@gmail.com>
2015-10-09 01:41:24 +0200

    Show appointment members as circles with initials

--- client/components/appointment-detail/MemberItem.jsx
+++ client/components/appointment-detail/MemberItem.jsx
@@ -2,13 +2,14 @@ MemberItem = React.createClass({
   propTypes: {
     firstname: React.PropTypes.string,
     lastname: React.PropTypes.string,
+    initials: React.PropTypes.string
   },
 
   render() {
     return (
-      <div className="list-item member">
-        { this.props.firstname } &nbsp; { this.props.lastname }
-      </div>
+      
+        <div className="circle">{ this.props.initials }</div>
+      
     );
   }
 });
