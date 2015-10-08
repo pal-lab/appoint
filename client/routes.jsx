41ea81968b0405bb0d3448881baeab41799c1efc
Jorrit Posor <jorrit.posor@mailbox.org>
2015-10-08 18:36:05 +0200

    fix

--- client/routes.jsx
+++ client/routes.jsx
@@ -8,8 +8,6 @@ const routes = (
   <Route name="root" handler={AppBody} path="/">
     <Route name="inboxPage" path="/inbox" handler={InboxPage} />
     <Route name="addMemberPage" path="/newappoint/addmembers" handler={AddMemberPage} />
-    
-    <Route name="appointmentPage" path="/appoint" handler={AppointmentPage} />
     <Route name="join" path="/join" handler={AuthJoinPage} />
     <Route name="signin" path="/signin" handler={AuthSignInPage} />
     <Route name="NewAppointmentPage" path="/NewAppointmentPage" handler={NewAppointmentPage} />
