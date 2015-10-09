57607074db9c1eab56437bae98069f22df34533f
Gregor Albrecht <gregor@albrechtpeters.de>
2015-10-09 19:38:10 +0200

    Tiny design update for inbox

--- client/components/AppointmentItem.jsx
+++ client/components/AppointmentItem.jsx
@@ -1,8 +1,19 @@
 const Link = ReactRouter.Link;
 
+const {
+  Navigation,
+  State
+} = ReactRouter;
+
 AppointmentItem = React.createClass({
+  mixins: [Navigation, State],
+
   propTypes: {
     appointment: React.PropTypes.object.isRequired,
+  },
+
+  openAppointment() {
+    this.transitionTo('appointmentpage', {appointment_id: this.props.appointment._id});
   },
 
 
@@ -10,18 +21,16 @@ AppointmentItem = React.createClass({
     let className = "list-item";
 
     return (
-      <li>
-        
-        <Link
-          className={ className }
-          key={ this.props.appointment._id }
-          to="appointmentpage"
-          params={{ appointment_id: this.props.appointment._id }}>
-            {this.props.appointment.purpose}
-        </Link>
-        
-        
-      </li>
+      <div className="row appointment-item" onClick={ this.openAppointment }>
+        <div className="col-md-4">
+          <h4>{ this.props.appointment.purpose }</h4>
+        </div>
+        <div className="col-md-8 col-full-height">
+          <p><span className="appointment-label">Location:</span> { this.props.appointment.location } </p>
+          <p><span className="appointment-label">Duration:</span> { this.props.appointment.duration } minutes </p>
+          <p><span className="appointment-label">Status:</span> { this.props.appointment.status } </p>
+        </div>
+      </div>
     );
   }
 });
