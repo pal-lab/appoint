fda619c7f6257950d49a6df2fe27539b2e8569c9
Gregor Albrecht <gregor@albrechtpeters.de>
2015-10-09 05:06:15 +0200

    Invite and unvite users

--- client/components/add-member/AddMemberItem.jsx
+++ client/components/add-member/AddMemberItem.jsx
@@ -4,26 +4,43 @@ AddMemberItem = React.createClass({
     appointment: React.PropTypes.object.isRequired
   },
 
-  getInitialState() {
-    return {
-      isInvited: false
-    };
+  // getInitialState() {
+  //   return {
+  //     isInvited: false
+  //   };
+  // },
+
+  toggleInvite() {
+    let invited = (_.indexOf(this.props.member.profile.invitations, this.props.appointment._id) > -1);
+    console.log('Invited?! ' + invited);
+    console.log('Invitation: ' + this.props.member.profile.invitations);
+
+    if(invited) {
+      console.log('UNinviting this user: ' + this.props.member._id);
+      Meteor.call("appointment/removeinvitee", this.props.appointment._id, this.props.member._id);
+    } else {
+      console.log('Inviting this user: ' + this.props.member._id);
+      Meteor.call("appointment/addinvitee", this.props.appointment._id, this.props.member._id);
+    }
+
+    invited = !invited;
+
   },
 
-  inviteUser() {
-    console.log('Inviting user with ID: ' + this.props.member._id)
-    Meteor.call("appointment/addinvitee", this.props.appointment._id, this.props.member._id);
-  },
+  // inviteUser() {
+  //   console.log('Inviting user with ID: ' + this.props.member._id)
+  //   Meteor.call("appointment/addinvitee", this.props.appointment._id, this.props.member._id);
+  // },
 
-  uninviteUser() {
-    console.log('Uninviting user with ID: ' + this.props.member._id);
-    Meteor.call("appointment/removeinvitee", this.props.appointment._id, this.props.member._id);
-  },
+  // uninviteUser() {
+  //   console.log('Uninviting user with ID: ' + this.props.member._id);
+  //   Meteor.call("appointment/removeinvitee", this.props.appointment._id, this.props.member._id);
+  // },
 
   render() {
     const memberName = (
       <div className="user">
-        { this.props.member.profile.firstname } { this.props.member.profile.lastname }
+        { this.props.member.profile.firstname } { this.props.member.profile.lastname } -> { this.props.member.profile.invitations }
       </div>
     )
 
@@ -39,16 +56,17 @@ AddMemberItem = React.createClass({
     }
 
     let listItem;
-    if(this.state.isInvited) {
+    const isUserInvited = (_.indexOf(this.props.member.profile.invitations, this.props.appointment._id) > -1);
+    if(isUserInvited) {
       listItem = (
-        <div className="list-item member" style={itemStyle} onClick={ this.uninviteUser }>
-          <span className="icon-check" style={iconStyle}></span>
+        <div className="list-item member" style={itemStyle} onClick={ this.toggleInvite }>
+          <span className="icon-plus" style={iconStyle}></span>
           { memberName }
         </div>
       )
     } else {
       listItem = (
-        <div className="list-item member" style={itemStyle} onClick={ this.inviteUser }>
+        <div className="list-item member" style={itemStyle} onClick={ this.toggleInvite }>
           <span className="icon-cross" style={iconStyle}></span>
           { memberName }
         </div>
