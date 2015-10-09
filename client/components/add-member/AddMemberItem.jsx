b9b5c51592944dbc4c232a01c64a937a2d8fe522
Gregor Albrecht <gregor@albrechtpeters.de>
2015-10-09 05:32:01 +0200

    Quick style update for links and buttons

--- client/components/add-member/AddMemberItem.jsx
+++ client/components/add-member/AddMemberItem.jsx
@@ -4,38 +4,18 @@ AddMemberItem = React.createClass({
     appointment: React.PropTypes.object.isRequired
   },
 
-  // getInitialState() {
-  //   return {
-  //     isInvited: false
-  //   };
-  // },
-
   toggleInvite() {
     let invited = (_.indexOf(this.props.member.profile.invitations, this.props.appointment._id) > -1);
-    console.log('Invited?! ' + invited);
-    console.log('Invitation: ' + this.props.member.profile.invitations);
 
     if(invited) {
-      console.log('UNinviting this user: ' + this.props.member._id);
       Meteor.call("appointment/removeinvitee", this.props.appointment._id, this.props.member._id);
     } else {
-      console.log('Inviting this user: ' + this.props.member._id);
       Meteor.call("appointment/addinvitee", this.props.appointment._id, this.props.member._id);
     }
 
     invited = !invited;
 
   },
-
-  // inviteUser() {
-  //   console.log('Inviting user with ID: ' + this.props.member._id)
-  //   Meteor.call("appointment/addinvitee", this.props.appointment._id, this.props.member._id);
-  // },
-
-  // uninviteUser() {
-  //   console.log('Uninviting user with ID: ' + this.props.member._id);
-  //   Meteor.call("appointment/removeinvitee", this.props.appointment._id, this.props.member._id);
-  // },
 
   render() {
     const memberName = (
@@ -49,14 +29,14 @@ AddMemberItem = React.createClass({
     if(isUserInvited) {
       listItem = (
         <div className="appnt-list-item list-item member" onClick={ this.toggleInvite }>
-          <span className="appnt-icon icon-plus"></span>
+          <span className="appnt-icon icon-cross"></span>
           { memberName }
         </div>
       )
     } else {
       listItem = (
         <div className="appnt-list-item list-item member" onClick={ this.toggleInvite }>
-          <span className="appnt-icon icon-cross"></span>
+          <span className="appnt-icon icon-plus"></span>
           { memberName }
         </div>
       )
