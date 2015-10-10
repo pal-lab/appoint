dfa74e56c08cb20d222b98ca79a5301c1283f63d
Gregor Albrecht <gregor@albrechtpeters.de>
2015-10-10 23:18:04 +0200

    Replace tabs in appointment with a tab bar
    The tab bar is a reusable component specific for the appointment details
    views.

--- client/components/appointment-detail/AppointmentDetails.jsx
+++ client/components/appointment-detail/AppointmentDetails.jsx
@@ -1,5 +1,4 @@
 /*jshint esnext: true */
-const Link = ReactRouter.Link;
 
 const {
   Navigation,
@@ -14,68 +13,22 @@ AppointmentDetails = React.createClass({
     invitedMembers: React.PropTypes.array.isRequired
   },
 
-  sendInvitations() {
-    console.log('yeahhh');
-    Meteor.call('appointment/invite', this.props.appointment._id);
-  },
-
-  navigateToAddMemberPage() {
-    this.transitionTo('addMemberPage', {appointment_id: this.props.appointment._id});
-  },
-
-  navigateToProposalPage() {
-   this.transitionTo('proposalpage', {appointment_id: this.props.appointment._id});
-  },
-
-  navigateToInboxPage() {
-   this.transitionTo('inboxPage');
-  },
-
   render() {
-
-      let invitationButton;
-      let addMemberButton;
-
-      if (this.props.appointment.initiator === Meteor.user()._id && this.props.appointment.status === "draft") {
-        invitationButton = (
-          <li>
-              <a onClick={ this.sendInvitations }>Send Invitations</a>
-          </li>
-          );
-      }
-
-      if (this.props.appointment.initiator === Meteor.user()._id && this.props.appointment.status === "draft") {
-        addMemberButton = (
-          <li>
-              <a onClick={ this.navigateToAddMemberPage }>Add Friendz</a>
-          </li>
-          );
-      }
-
-
-      return (
+      let renderObject = (
         <div className="page appointment-details">
-          <ul className="nav nav-pills nav-justified">
-            <li>
-              <a onClick = { this.navigateToInboxPage }>
-                Inbox
-              </a>
-            </li>
-            <li>
-              <a onClick={ this.navigateToProposalPage }>Proposals</a>
-            </li>
-            { addMemberButton }
-            { invitationButton }
-          </ul>
-
+          <AppointmentTabBar
+            appointment = { this.props.appointment }
+            />
           <AppointmentSettings
-            appointment={this.props.appointment}/>
-
+            appointment = { this.props.appointment }
+            />
           <AppointmentMembers
-            invitedMembers={this.props.invitedMembers}
-            appointment={this.props.appointment}/>
-
+            invitedMembers = { this.props.invitedMembers }
+            appointment = { this.props.appointment }
+            />
         </div>
       );
+
+      return renderObject;
   }
 });
