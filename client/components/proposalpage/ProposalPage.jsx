0d23bcf0324f472eadba2dd9c634fa6b6a264267
Gregor Albrecht <gregor@albrechtpeters.de>
2015-10-11 02:01:50 +0200

    Fixed proposal list and updated the styling
    pretty cool eh

--- client/components/proposalpage/ProposalPage.jsx
+++ client/components/proposalpage/ProposalPage.jsx
@@ -12,10 +12,49 @@ ProposalPage = React.createClass({
   getMeteorData() {
     // Get list ID from ReactRouter
     const appointment_id = this.getParams().appointment_id;
-    const proposalsSubHandle = Meteor.subscribe("appointmentproposal", appointment_id);
+    const proposalsSubHandle = Meteor.subscribe("appointmentevents", appointment_id);
+    var proposals = [];
+    AppointmentEvents.find({
+      type: 'proposed'
+    }).forEach(function(prop) {
+      var vote = AppointmentEvents.find({
+        $and: [
+          {
+            date: prop.date,
+            account: Meteor.userId()
+          }, {
+            $or: [
+              {
+                type: 'accepted'
+              }, {
+                type: 'rejected'
+              }
+            ]
+          }
+        ]
+      }, {
+        sort: {
+          createdAt: -1
+        },
+        limit: 1
+      }).fetch();
+      if (vote.length > 0) {
+        proposals.push({
+          date: prop.date,
+          vote: vote[0].type
+        });
+      } else {
+        proposals.push({
+          _id: vote._id,
+          date: prop.date,
+          vote: 'notVoted'
+        });
+      }
+    });
+
     return {
-      proposals: AppointmentProposals.find().fetch(),
       appointment: Appointments.findOne({ _id: appointment_id }),
+      proposals: proposals
     };
   },
 
@@ -24,12 +63,14 @@ ProposalPage = React.createClass({
       <div className="page lists-show">
         <HeaderBar
           status={"ProposalPage"}
-          showLoadingIndicator={this.data.tasksLoading}/>
+          showLoadingIndicator={this.data.tasksLoading}
+          />
 
         <div className="content-scrollable">
           <ProposalList
             proposals={this.data.proposals}
-            appointment = { this.data.appointment }/>
+            appointment = { this.data.appointment }
+            />
         </div>
       </div>
     );
