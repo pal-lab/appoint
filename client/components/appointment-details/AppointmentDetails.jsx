9b606dd0e27ea6210e74a07a9168300edeefa2a1
Gregor Albrecht <gregor@albrechtpeters.de>
2015-10-11 06:39:12 +0200

    Shiny new buttons to accept or decline inside the details view
    Also slightly refactored the check for own appointments.

--- client/components/appointment-details/AppointmentDetails.jsx
+++ client/components/appointment-details/AppointmentDetails.jsx
@@ -13,7 +13,41 @@ AppointmentDetails = React.createClass({
     invitedMembers: React.PropTypes.array.isRequired
   },
 
+  getInitialState() {
+    return {
+      currentUser: Meteor.user()
+    };
+  },
+
+  acceptAppointment(){
+    Meteor.call('appointment/approve', this.props.appointment._id);
+  },
+
+  declineAppointment(){
+    Meteor.call('appointment/decline', this.props.appointment._id);
+  },
+
+  hasAcknowledged() {
+    return Meteor.appointmentQueries.hasAcknowledgment(this.props.appointment._id);
+  },
+
+  isOwnAppointment() {
+    return (this.state.currentUser._id === this.props.appointment.initiator);
+  },
+
   render() {
+      let answerInvitationButtons = null;
+      if(!this.hasAcknowledged() && !this.isOwnAppointment()) {
+        answerInvitationButtons = (
+          <div style={{width: '100%', textAlign: 'center'}}>
+            <button onClick={ this.acceptAppointment } className="btn btn-primary">Einladung annehmen <span className="icon-check"></span></button>
+            <br/>
+            <button onClick={ this.declineAppointment } className="btn btn-info">Einladung ablehnen <span className="icon-cross"></span></button>
+            <br/><br/>
+          </div>
+        );
+      };
+
       let renderObject = (
         <div className="page appointment-details">
           <AppointmentTabBar
@@ -22,6 +56,9 @@ AppointmentDetails = React.createClass({
           <AppointmentSettings
             appointment = { this.props.appointment }
             />
+
+          { answerInvitationButtons }
+
           <AppointmentMembers
             invitedMembers = { this.props.invitedMembers }
             appointment = { this.props.appointment }
