3b9ba82e9358842b6e65b3e969a44d19e949b074
Jorrit Posor <jorrit.posor@mailbox.org>
2015-11-06 09:26:28 +0100

    Okay, it's fixed now

--- client/components/appointment-details/AppointmentPage.jsx
+++ client/components/appointment-details/AppointmentPage.jsx
@@ -15,10 +15,9 @@ AppointmentPage = React.createClass({
     let appointmentInitiator = null;
     if(Meteor.subscriptionManager.ready){
       appointmentInitiator = Appointments.findOne({ _id: appointment_id }).initiator;
-      scheduledEvent= Meteor.appointmentQueries.scheduled(appointment_id);
     }
     return {
-      scheduledEvent: scheduledEvent,
+      scheduledEvent: scheduledEvent= Meteor.appointmentQueries.scheduled(appointment_id),
       appointment: Appointments.findOne({ _id: appointment_id }),
       invitedMembers: Meteor.users.find({ '_id': { $ne: appointmentInitiator } , 'profile.invitations': { $in: [ appointment_id ] }}, {
         emails: 1,
@@ -29,8 +28,6 @@ AppointmentPage = React.createClass({
 
   render() {
     const appointment = this.data.appointment;
-
-    console.log(this.data.scheduledEvent);
 
     if (! appointment) {
       return <AppNotFound />;
